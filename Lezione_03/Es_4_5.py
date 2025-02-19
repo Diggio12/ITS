22024bed5e9b2d03d1f1b7684e0c96201644b59c
Diggio12 <mattia.digiorgio.mdg@gmail.com>
2025-02-19 12:52:46 +0100

    Es_python

---
+++ Lezione_03/Es_4_5.py
@@ -0,0 +1,13 @@
+one_million:list = []
+i:int = 1
+
+while i < 1000000 + 1:
+    one_million.append(i)
+    print(one_million[i - 1])
+    i += 1
+    
+
+a:int = min(one_million)
+b:int = max(one_million)
+c:int = sum(one_million)
+print(a, b, c)
