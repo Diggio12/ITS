c98d0026d3f433cd0fa3ffbb1c56ade59ee28e9a
Diggio12 <mattia.digiorgio.mdg@gmail.com>
2025-02-25 16:52:23 +0100

    Es_Match_Statement

---
+++ Lezione_03/Es_3C_3.py
@@ -0,0 +1,16 @@
+oggetti:list = []
+
+while len(oggetti) < 3:
+    oggetti.append(input('Inserire oggetto: '))
+
+match oggetti:
+    case ['penna', 'matita', 'quaderno']:
+        print('Materiale scolastico')
+    case ['pane', 'latte', 'uova']:
+        print('Prodotti alimentari')
+    case ['sedia', 'tavolo', 'armadio']:
+        print('Mobili')
+    case ['telefono', 'computer', 'tablet']:
+        print('Dispositivi elettronici')
+    case _:
+        print('Categoria sconosciuta')
