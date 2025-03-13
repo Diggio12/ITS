e441a57d0ef04f46c897951c5f1af75ac6f2e28a
Diggio12 <mattia.digiorgio.mdg@gmail.com>
2025-03-13 16:28:26 +0100

    Diagrammi_in_Python

--- Lezione_03/Diagrammi_in_Python15.py
+++ Lezione_03/Diagrammi_in_Python15.py
@@ -6,10 +6,9 @@ if n >= 1 and n <= 100:
     if n % 2 == 0:    
         for i in range (0, n + 1, 2):
             somma_p += i
-    elif n % 2 == 1:
-        for x in range (1, n+ 1, 2):
-            somma_d = somma_d + x
+    for x in range (1, n+ 1, 2):
+        somma_d = somma_d + x
 elif n == 0 or n < 0:
     print('Errore')
 
-print(somma_p, somma_d)
+print(f'Totale somma dei numeri pari è: {somma_p}\nTotale somma dei numeri dispari è: {somma_d}')
