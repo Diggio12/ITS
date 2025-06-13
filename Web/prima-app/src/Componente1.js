2a7ba8fbddc266589e18286ed41c76270111194f
Diggio12 <mattia.digiorgio.mdg@gmail.com>
2025-06-13 13:59:00 +0200

    web

---
+++ Web/prima-app/src/Componente1.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Componente1 = (props) => {
+    const anni = 48;
+  return (
+    <div
+    style={{
+        color: 'red',
+        fontWeight: '600',
+        margin: '15px',
+        border: '1px #000 solid',
+        padding: '15px'
+    }}
+    >
+    <div>Componente1 {props.children}</div>
+    <Anagrafica></Anagrafica>
+    <Messaggio></Messaggio>
+    </div>
+  )
+}
+
+const Anagrafica=()=>{
+    return(
+        <div>Anagrafica</div>
+    )
+}
+
+const Messaggio=()=>{
+    return(
+        <div>Messaggio</div>
+    )
+}
+
+
+export default Componente1
+	
