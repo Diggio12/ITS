36c112682933a31b48558fea96876f3c3c75940a
Diggio12 <mattia.digiorgio.mdg@gmail.com>
2025-10-06 17:00:52 +0200

    es

--- DataBase/Accademia/accademia6_query.sql
+++ DataBase/Accademia/accademia6_query.sql
@@ -13,3 +13,15 @@ where stipendio >= 40000;
 select count(*)
 from Progetto
 where budget > 50000 and fine < current_date
+
+
+---Qual è la media, il massimo e il minimo delle ore delle attività relative al progetto "Pegasus" ?
+select avg(oreDurata), min(oreDurata), max(oreDurata)
+from Progetto as pr, AttivitaProgetto as ap
+where pr.id = ap.progetto and pr.nome = 'Pegasus';
+
+
+---Quali sono le medie, i massimi e i minimi delle ore giornaliere dedicate al progetto ‘Pegasus’ da ogni singolo docente?
+select avg(oreDurata), min(oreDurata), max(oreDurata)
+from Progetto as pr, AttivitaProgetto as ap, Persona as pers 
+where pr.id = ap.progetto and pr.nome = 'Pegasus' and pers.id = ap.persona;
